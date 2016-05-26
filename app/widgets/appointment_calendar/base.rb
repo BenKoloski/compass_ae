4550f73d27e13ce9ff0cc22b79ca9474194340ff
Benjamin Koloski <bkoloski@truenorthtechnology.com>
2016-05-26 11:21:34 -0400

    asdf

--- app/widgets/appointment_calendar/base.rb
+++ app/widgets/appointment_calendar/base.rb
@@ -1,7 +1,7 @@
 module Widgets
   module AppointmentCalendar
     class Base < ErpApp::Widgets::Base
-  
+
       def index
         render :view => :index
       end
@@ -22,30 +22,42 @@ module Widgets
 
         return :json => { 
           success: true,
-          my_appointments: my_appointments,
-          not_my_appointments: not_my_appointments,
-          availability_slots: availability_slots
+          my_appointments: my_appointments || [],
+          not_my_appointments: not_my_appointments || [],
+          availability_slots: availability_slots || []
         }
       end
 
       def create_event
         event = CalendarEvent.new
-        event.starttime = DateTime.new params[:start]
-        event.endtime = DateTime.new params[:end]
+        params[:start] = params[:start].map {|el| el.to_i}
+        params[:end] = params[:end].map {|el| el.to_i}
+        event.starttime = DateTime.new params[:start][0], params[:start][1], params[:start][2], params[:start][3], params[:start][4]
+        event.endtime = DateTime.new params[:end][0], params[:end][1], params[:end][2], params[:end][3], params[:end][4]
+        event.calendar_event_type_id = CalendarEventType.find_by_internal_identifier('cal_appointment').id
+
+        cepr = CalEvtPartyRole.new
+        cepr.party_id = User.find(current_user).party.id
+        cepr.role_type_id = RoleType.find_by_internal_identifier('appointment_requester').id
+        cepr.calendar_event = event
+
+        event.save
+        cepr.save
+
         return :json => {success: true}
       end
-  
+
       #should not be modified
       #modify at your own risk
       def locate
         File.dirname(__FILE__)
       end
-  
+
       class << self
         def title
           "Appointment Calendar"
         end
-    
+
         def widget_name
           File.basename(File.dirname(__FILE__))
         end
