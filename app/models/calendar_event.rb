7cf186485888e92b67fb038345f91327465c7d94
Benjamin Koloski <bkoloski@truenorthtechnology.com>
2016-06-01 12:52:01 -0400

    a

--- app/models/calendar_event.rb
+++ app/models/calendar_event.rb
@@ -1,7 +1,8 @@
 class CalendarEvent < ActiveRecord::Base
 	belongs_to :calendar_event_type
 	has_many :cal_evt_party_roles 
-	
+	has_many :parties, through: :cal_evt_party_roles
+
 	def to_date_range 
 		[starttime, endtime]
 	end
